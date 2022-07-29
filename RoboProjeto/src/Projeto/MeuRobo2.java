f30e86b69f76bcb82b1346594b4b9b64b083d5df
Willian Murayama <willianyos13@gmail.com>
2022-07-28 23:56:46 -0300

    Criação do projeto

---
+++ RoboProjeto/src/Projeto/MeuRobo2.java
@@ -0,0 +1,46 @@
+package Projeto;
+
+import robocode.*;
+
+public class MeuRobo2 extends RoboBasico {
+
+    int forca_tiro = 1;
+
+    public void atirar() {
+        fire(1);
+    }
+
+    public void atirar(int forca) {
+        fire(forca);
+    }
+
+    @Override
+    public void configurarRobo() {
+        configurarCor("Corinthians");
+    }
+
+    @Override
+    public void mover() {
+        setAhead(200);
+        setTurnRight(30);
+        turnGunRight(10);
+    }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent e) {
+        atirar(forca_tiro);
+        if (forca_tiro <= 3) {
+            forca_tiro++;
+        } else {
+            forca_tiro = 1;
+        }
+        atirar();
+    }
+
+    @Override
+    public void onHitWall(HitWallEvent e) {
+// quando bater na parede, recuar 50 e virar 45 graus
+        back(50);
+        turnRight(45);
+    }
+}
---
+++ RoboProjeto/src/Projeto/RoboBasico.java
@@ -0,0 +1,66 @@
+package Projeto;
+
+import java.awt.Color;
+import robocode.*;
+
+public class RoboBasico extends AdvancedRobot {
+
+    public void configurarCor(String time) {
+        if (null != time)//colorir o tanque
+        {
+            switch (time) {
+                case "São Paulo" -> {
+                    setBodyColor(Color.red);
+                    setGunColor(Color.black);
+                    setRadarColor(Color.black);
+                    setScanColor(Color.white);
+                    setBulletColor(Color.white);
+                }
+                case "Palmeiras" -> {
+                    setBodyColor(Color.green);
+                    setGunColor(Color.white);
+                    setRadarColor(Color.green);
+                    setScanColor(Color.white);
+                    setBulletColor(Color.green);
+                }
+                case "Corinthians" -> {
+                    setBodyColor(Color.white);
+                    setGunColor(Color.black);
+                    setRadarColor(Color.black);
+                    setScanColor(Color.white);
+                    setBulletColor(Color.white);
+                }
+                case "Santos" -> {
+                    setBodyColor(Color.black);
+                    setGunColor(Color.white);
+                    setRadarColor(Color.white);
+                    setScanColor(Color.black);
+                    setBulletColor(Color.black);
+                }
+                default -> {
+                }
+            }
+        }
+    }
+
+    public void mover() {
+        ahead(100);
+        turnGunRight(180);
+        back(100);
+        turnGunRight(180);
+    }
+
+    public void configurarRobo() {
+        configurarCor("São Paulo");
+    }
+
+    @Override
+    public void run() {
+        configurarRobo();
+// Robot main loop
+        while (true) {
+            mover();
+        }
+    }
+
+}
