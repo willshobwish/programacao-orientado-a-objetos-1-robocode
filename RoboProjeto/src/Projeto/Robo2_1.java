d5ea03a26c296ee610d59db83a207b33072bb1b7
Willian Murayama <willianyos13@gmail.com>
2022-07-30 09:50:39 -0300

    Adição de comentário
    Adição da biblioteca

--- RoboProjeto/src/Projeto/Robo2_1.java
+++ RoboProjeto/src/Projeto/Robo2_1.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+//Andressa Yida Pinheiro de Souza
+//Willian Yoshio Murayama
 package Projeto;
 
-import robocode.HitByBulletEvent;
+import robocode.*;
 
-/**
- *
- * @author Willian
- */
 public class Robo2_1 extends MeuRobo2 {
 
     @Override
