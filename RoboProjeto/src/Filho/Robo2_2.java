3915c31110b857a2e29d6dbf1ad4add16140e956
Willian Murayama <willianyos13@gmail.com>
2022-08-08 14:58:56 -0300

    Correção do Robo 2 2
    Correção do setFire dependendo da distancia
    Renomeação do pacote

--- RoboProjeto/src/Filho/Robo2_2.java
+++ RoboProjeto/src/Filho/Robo2_2.java
@@ -1,22 +1,26 @@
 //Andressa Yida Pinheiro de Souza
 //Willian Yoshio Murayama
-package Projeto;
+package Filho;
 
 import Pai.MeuRobo2;
 import robocode.*;
 
 public class Robo2_2 extends MeuRobo2 {
 
-    protected double distance;
+    private double distance;
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        double distance = e.getDistance();
+        distance = e.getDistance();
     }
 
     @Override
-    public void run() {
-        if (distance < 50) {
-
+    public void mover() {
+        if (distance >= 0 && distance < 50) {
+            setFire(3);
+        } else if (distance >= 50 && distance < 100) {
+            setFire(2);
+        } else {
+            setFire(1);
         }
 // Robot main loop
         while (true) {
